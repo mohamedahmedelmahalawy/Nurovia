8e2963dbfd723a784893986ae78f2ac9cbb5f3c2
mizomico <aroky.me@gmail.com>
2026-05-30 11:12:10 +0300

    update all files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import AiTools from "../components/AiTools";
+import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import Plan from "../components/Plan";
 import Testimonial from "../components/Testimonial";
 
 export default function Home() {
@@ -10,6 +12,8 @@ export default function Home() {
       <Hero />
       <AiTools />
       <Testimonial />
+      <Plan />
+      <Footer />
     </div>
   );
 }
