7ea0cc57e9b4a6fd627ad1cdb692a841654f3cb5
mizomico <aroky.me@gmail.com>
2026-06-01 13:02:50 +0300

    update dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+
 export default function Dashboard() {
+  const [creations, setCreations] = useState([]);
+  const getDashboaedData = async () => {};
   return <div>Dashboard</div>;
 }
